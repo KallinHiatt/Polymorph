449eb28ff837be18b0cf058ae9a1cd8a39a98964
KallinHiatt <kallin.hiatt@ucas-edu.net>
2024-12-02 22:04:17 +0000

    THIS IS NO LONGER POLYMORPH

---
+++ main.py
@@ -0,0 +1,44 @@
+import pygame
+import sys
+from game import Game
+pygame.init()
+screen = pygame.display.set_mode((400, 720))
+clock = pygame.time.Clock()
+game = Game('img/bird.png', 'img/pipe.png', 'img/background.png', 'img/ground.png', 'img/bill.png')
+game.resize_images()
+SPAWNPIPE = pygame.USEREVENT
+SPAWNBILL = pygame.USEREVENT
+pygame.time.set_timer(SPAWNPIPE, 1000)
+pygame.time.set_timer(SPAWNBILL, 5000)
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and game.active:
+                game.flap()        
+            if event.key == pygame.K_SPACE and game.active == False:
+                game.restart()
+        if event.type == SPAWNPIPE:
+            game.add_pipe() 
+        if event.type == SPAWNBILL:
+            game.add_bill()
+    game.show_background(screen)
+    if game.active:
+        game.show_bird(screen)
+        game.update_bird()
+        game.move_pipe()
+        game.show_pipes(screen)
+        game.check_collision()
+        game.move_bill()
+        game.show_bill(screen)
+        game.update_score()
+        game.show_score("playing", screen, (255,255,255))
+    else:
+        game.game_over(screen, (255, 255, 255))
+    game.show_ground(screen)
+    game.move_ground()
+    pygame.display.update()
+    clock.tick(120)
+     
